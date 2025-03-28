691bbbe31017b34c86705ce6305528301610d6b5
minkim-korea <km24778@gmail.com>
2025-03-28 09:11:02 +0900

    수정

--- python/home0328.py
+++ python/home0328.py
@@ -2,20 +2,24 @@
 # 랜덤숫자 
 
 import random
-n = []
-n = 0 
+num = []
 
-ran_random = random.randint(1,100)
-in_random = int(input("숫자를입력하세요:"))
+ran_num= random.randint(1,100)
+n =0 
 
-for n in range(1,10):
-      .append(in_random)
-    if ran_random==in_random :
-        print("정답입니다 {}".format(ran_random))
-    elif ran_random>in_random:
-        print("{}보다작은값을입력하세요".format(in_random))
+for n in range(1,6):
+    in_num = int(input("숫자를입력하세요:"))
+    num.append(in_num)
+    if ran_num==in_num :
+        print("정답입니다. {}".format(ran_num))
+        break
+    elif ran_num>in_num:
+        print("{}보다 큰값을입력하세요".format(in_num))
     else :
-        print("{}보다큰값을입력하세요".format(in_random))
+        print("{}보다작은값을입력하세요".format(in_num))
         
+print("도전회수:{}".format(n))
+print("입력숫자:{}".format(num))
+print("랜덤숫자{}.".format(ran_num))
 
     
